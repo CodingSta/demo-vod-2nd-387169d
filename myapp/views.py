18d4538de93e75e684cc6d9fa8a4650f42d9f1f8
Chinseok Lee <me@askcompany.kr>
2018-12-19 10:35:51 +0700

    EP02: Base Views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView, RedirectView
 
-# Create your views here.
+
+# def myview(request):
+#     context = {
+#         'hello': 'world',
+#         'ip': request.META['REMOTE_ADDR'],
+#     }
+#     return render(request, 'myapp/myview.html', context)
+
+
+class MyTemplateView(TemplateView):
+    template_name = 'myapp/myview.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'hello': 'world',
+            'ip': self.request.META['REMOTE_ADDR'],
+        })
+        return context
+
+myview = MyTemplateView.as_view()
+
+
+# def myview2(request):
+#     return redirect('https://m.naver.com')
+
+myview2 = RedirectView.as_view(url='https://m.naver.com')
